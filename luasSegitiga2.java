fa5636a48ce446c68c69bdca47629172b6587050
iswhyd <iswahyudiiskandar@gmail.com>
2023-10-30 16:25:34 +0700

    Perubahan

--- luasSegitiga2.java
+++ luasSegitiga2.java
@@ -1,30 +1,36 @@
-package Modul3Smstr3;
+package Modul3Smstr3Prak;
 
-/*
-* Disini saya membuat class luasSegitiga2
-* yang dimana class ini bertujuan untuk menghitung luas dari sebuah segitiga
-* */
+/**
+ *
+ */
 public class luasSegitiga2 {
 
-    /*
-    * Disini terdapat 2 variable untuk menampung angka dari alas dan tinggi
-    * */
+
     private int num1;
     private int num2;
 
+    /**
+     *
+     * @param num1
+     * @param num2
+     */
     public luasSegitiga2(int num1 , int num2){
         this.num1=num1;
         this.num2=num2;
     }
-    /*
-     * dibawah ini merupakan method rumus dari menghitung luas segitiga
-     * */
+
+    /**
+     *
+     * @return
+     */
     public double calculateTriangle(){
         return 0.5 * num2 * num2;
     }
-    /*
-     * dibawah ini method untuk menjalankan class tersebut
-     * */
+
+    /**
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         luasSegitiga2 luassegitiga2 = new luasSegitiga2(10,10);
 
